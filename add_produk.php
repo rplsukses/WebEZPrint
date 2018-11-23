393af36be06d98656204ee6522084d0313fc8e91
unknown <Fauziahikrar>
2018-11-23 16:44:18 +0700

    include header footer

--- add_produk.php
+++ add_produk.php
@@ -1,49 +1,13 @@
 <?php
     include_once 'admin/api/config/database.php';
-
     $database = new Database();
     $db = $database->getConnection();
+
+    include 'include/header.php';
+    include 'include/footer.php';
 ?>
 
-<!DOCTYPE html>
-<!--> <html class="no-js" lang="en"> <!--<![endif]-->
-<head>
-    <link href="images/logo.png" rel="print-icon" sizes="100x100">
-    <link href="images/logo.png" rel="icon" type="image/png">
-    <title>EZPrint Mitra Center</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="templatemo">
-    <!-- 
-    Compass Template
-    http://www.templatemo.com/tm-454-compass
-    -->
-    <meta charset="UTF-8">
-
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-
-    <!-- CSS Bootstrap & Custom -->
-    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/templatemo-misc.css">
-    <link rel="stylesheet" href="css/animate.css">
-    <link rel="stylesheet" href="css/templatemo-main.css">
-        
-    <!-- Favicons -->
-    <link rel="shortcut icon" href="images/ico/favicon.ico">
-    
-    <!-- JavaScripts -->
-    <script src="js/jquery-1.10.2.min.js"></script>
-    <script src="js/modernizr.js"></script>
-    <!--[if lt IE 8]>
-    <div style=' clear: both; text-align:center; position: relative;'>
-            <a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" alt="" /></a>
-        </div>
-    <![endif]-->
-</head>
-<body>
-    
+<body> 
     <div id="home">
         <div class="site-header">
             <div class="top-header">
@@ -166,34 +130,6 @@
             </div> <!-- /.row -->
         </div> <!-- /.container -->
     </div> <!-- /.site-footer -->
-
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-    <script src="js/plugins.js"></script>
-    <script src="js/jquery.lightbox.js"></script>
-    <script src="js/custom.js"></script>
-    <script type="text/javascript">
-
-        function initialize() {
-          var mapOptions = {
-              scrollwheel: false,
-            zoom: 15,
-            center: new google.maps.LatLng(13.758468,100.567481)
-          };
-
-          var map = new google.maps.Map(document.getElementById('map-canvas'),
-              mapOptions);
-        }
-
-        function loadScript() {
-          var script = document.createElement('script');
-          script.type = 'text/javascript';
-          script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
-              'callback=initialize';
-          document.body.appendChild(script);
-        }
-
-        window.onload = loadScript;
-    </script>
 
 </body>
 </html>
---
+++ include/footer.php
@@ -0,0 +1,27 @@
+<script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/jquery.lightbox.js"></script>
+    <script src="js/custom.js"></script>
+    <script type="text/javascript">
+
+        function initialize() {
+          var mapOptions = {
+              scrollwheel: false,
+            zoom: 15,
+            center: new google.maps.LatLng(13.758468,100.567481)
+          };
+
+          var map = new google.maps.Map(document.getElementById('map-canvas'),
+              mapOptions);
+        }
+
+        function loadScript() {
+          var script = document.createElement('script');
+          script.type = 'text/javascript';
+          script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
+              'callback=initialize';
+          document.body.appendChild(script);
+        }
+
+        window.onload = loadScript;
+    </script>
