a6f549d1e0bcaa57456af503026902eeb4a389ca
novariita <novarita10.nr@gmail.com>
2018-11-23 15:47:35 +0700

    include

--- admin/include/header.php
+++ admin/include/header.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<link href="assets/img/logo.png" rel="print-icon" sizes="100x100">
-	<link href="assets/img/logo.png" rel="icon" type="image/png">
+	<link href="assets/img/logo_3.png" rel="print-icon" sizes="100x100">
+	<link href="assets/img/logo_3.png" rel="icon" type="image/png">
 	<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
 	<title>EZPrint</title>
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport'><!--     Fonts and icons     -->
