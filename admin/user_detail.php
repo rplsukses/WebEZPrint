47c87f816e3f7d25c668272f9a417539035fa17c
novariita <novarita10.nr@gmail.com>
2018-12-28 11:17:35 +0700

    user detail backend

--- admin/user_detail.php
+++ admin/user_detail.php
@@ -3,7 +3,11 @@
 	include 'core/init.php';
   include 'include/header.php';
   include 'include/footer.php';
-	include 'include/sidebar.php';
+  include 'include/sidebar.php';
+  
+    //Query to get id 
+	$ambil=$conn->query("SELECT * FROM user WHERE id_user='$_GET[id]'");
+	$pecah= $ambil->fetch_assoc();
 ?>
 
 
@@ -12,7 +16,7 @@
   <div class="content">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-md-12">
+      <div class="col-md-6">
           <div class="card">
             <div class="card-header card-header-primary">
               <center><h3 class="card-title">Detail User</h3></center>
@@ -20,35 +24,34 @@
             <div class="card-body">
               <div class="table-responsive">
                 <table class="table">
-                  <thead class=" text-primary">
+                  <thead>
                     <tr>
-                      <th>ID User :</th>
+                      <td>Nama : <?php echo $pecah['nama']; ?></td>
+                    </tr>
+                    <tr>
+                      <td>Telepon : <?php echo $pecah['telepon'];?></td>
+                    </tr>
+                    <tr>
+                      <td>Email : <?php echo $pecah['email'];?></td>
+                    </tr>
+                    <tr>
+                      <td>Last Seen : <?php echo $pecah['last_seen'];?></td>
                     </tr>
                   </thead>
-                  <tbody>
-                    <tr>
-                      <td>Nama :</td>
-                    </tr>
-                    <tr>
-                      <td>Password :</td>
-                    </tr>
-                    <tr>
-                      <td>Telepon :</td>
-                    </tr>
-                    <tr>
-                      <td>Email :</td>
-                    </tr>
-                    <tr>
-                      <td>Last Seen :</td>
-                    </tr>
-										<tr>
-											<td>Foto :</td>
-										</tr>
-                  </tbody>
                 </table>
               </div>
             </div>
           </div>
+        </div>
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-header card-header-primary">
+              <center><h3 class="card-title">Foto User</h3></center>
+            </div>
+            <div class="card-body">
+            <center><img src="icon/<?php echo $pecah['foto']; ?>" width="210" height="210"></center>
+              </div>
+            </div>
         </div>
       </div>
     </div>
