56e27eba71472d630844a56d9a5dbb899d513b8f
imaisnaini <imaisnaini@gmail.com>
2019-01-06 03:31:17 +0700

    fixing api register

--- admin/api/user/register.php
+++ admin/api/user/register.php
@@ -32,11 +32,11 @@
                 while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
                     extract($row);
                     
-                    $user_arr["data"]["id_user"] = $id_user;
-                    $user_arr["data"]["nama"] = $nama;
-                    $user_arr["data"]["email"] = $email;
-                    $user_arr["data"]["foto"] = $foto;
-                    $user_arr["data"]["telepon"] = $telepon;
+                    $user_arr["id_user"] = $id_user;
+                    $user_arr["nama"] = $nama;
+                    $user_arr["email"] = $email;
+                    $user_arr["foto"] = $foto;
+                    $user_arr["telepon"] = $telepon;
                     $user_arr["error"] = FALSE;
                     $user_arr["message"] = "Register sukses!";
                 }
