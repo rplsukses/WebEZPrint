28f452e8e2dade5645aeecd67b7d3980a192771c
novariita <novarita10.nr@gmail.com>
2019-01-08 04:14:18 +0700

    mitra done

--- edit_produk.php
+++ edit_produk.php
@@ -2,11 +2,20 @@
     include 'admin/api/config/database.php';
     include 'admin/core/init.php';
     include 'include/header.php';
+    
+    // Session
     session_start();
+    if(!isset($_SESSION['login_mitra'])) {
+        header('location:index.php');
+    }else {
+        $login_mitra = $_SESSION['login_mitra'];
+    }
+
+    //Connect To Database
     $database = new Database();
     $db = $database->getConnection();
 
-    //QUERY SELECT
+    //QUERY SELECT TO EDIT PRODUK
     if(isset($_GET['id']) && !empty($_GET['id'])){
         $id_produk=$_GET['id'];
         $query = ("SELECT * FROM produk, kategori
@@ -16,7 +25,7 @@
         $result = mysqli_query($conn, $query);
     }
 
-    //QUERY POST
+    //QUERY UPDATE PRODUK
     if(isset($_POST['save'])){
         $id_kategori = $_POST['id_kategori'];
         $ukuran = $_POST['ukuran'];
@@ -31,7 +40,6 @@
                 harga='".$harga."' 
                 WHERE produk.id_produk=".$id_produk;
 
-    
         $update = mysqli_query($conn, $query);
         if($update){
             header('Location: produk.php?status=0');
@@ -42,8 +50,8 @@
 
 ?>
 
-<body>
-    
+<!-- Content -->
+<body>    
     <div id="home">
         <div class="site-header">
             <div class="top-header">
@@ -52,72 +60,68 @@
                         <div class="col-md-6 col-sm-6">
                             <div class="left-header lead">
                             <a href="welcome.php"><img src="images/logoo2.png" width="280px" height="50px"></a>                               
-                            </div> <!-- /.left-header -->
-                        </div> <!-- /.col-md-6 -->
-                    </div> <!-- /.row -->
-                </div> <!-- /.container -->
-            </div> <!-- /.top-header -->
-            
-<div class="main-header">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="main-header">
                 <div class="container">
                     <div class="row">
                         <div class="text-center">
                             <div class="logo">
                                 <h1><a href="#" title="Dreri"> EDIT PRODUK</a></h1>
-                            </div> <!-- /.logo -->
-                        </div> <!-- /.col-md-4 -->
+                            </div>
+                        </div>
                         <div class="col-md-8 col-sm-8 col-xs-6">
-                            <div class="menu text-right hidden-sm hidden-xs">
-                                
-                            </div> <!-- /.menu -->
-                        </div> <!-- /.col-md-8 -->
-                    </div> <!-- /.row -->
+                            <div class="menu text-right hidden-sm hidden-xs">                            
+                            </div>
+                        </div>
+                    </div>
                     <div class="responsive-menu text-right visible-xs visible-sm">
                         <a href="#" class="toggle-menu fa fa-bars"></a>
-                        <div class="menu">
-                            
-                        </div> <!-- /.menu -->
-                    </div> <!-- /.responsive-menu -->
-                </div> <!-- /.container -->
-            </div> <!-- /.header -->
-        </div> <!-- /.site-header -->
-    </div> <!-- /#home -->
+                        <div class="menu"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div id="services"> 
         <div>
-            <div class="title-section text-center">
-               
-            </div> <!-- /.title-section -->
-          <div class="container lead">
-         
-            <div class="row">
-              
-                        
-              <!-- Content -->
-                   <div class="col-md-12">
+            <div class="title-section text-center"></div>
+                <div class="container lead">
+                    <div class="row">
+                        <div class="col-md-12">
                         <form action="" method="POST">
+                        
                         <?php
                             while($row=$result->fetch_assoc()){
                         ?>
-                          <div class="form-group row">
-                            <label for="colFormLabel" class="col-sm-2 col-form-label">ID Produk</label>
+
+                        <div class="form-group row">
+                        <label for="colFormLabel" class="col-sm-2 col-form-label">ID Produk</label>
                             <div class="col-sm-10">
                                 <input type="text" name="id_produk" class="form-control" id="colFormLabel" value="<?=$row['id_produk'];?>"  disabled>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="colFormLabel" class="col-sm-2 col-form-label">Kategori</label>
+                        <label for="colFormLabel" class="col-sm-2 col-form-label">Kategori</label>
                             <div class="col-sm-10">
                                 <select class="col-sm-12" name="id_kategori">
+                                
                                 <?php 
                                 // QUERY KATEGORY
                                 $query_kat = "SELECT * FROM kategori";
                                 $kategori = mysqli_query($conn, $query_kat);
                                 while($row_kat = $kategori->fetch_assoc()){
                                 ?>
+                                
                                 <option name="id_kategori" value="<?=$row_kat['id_kategori']?>"><?=$row_kat['nama'];?></option>
+                                
                                 <?php
                                 }
                                 ?>
+                                
                                 </select>
                             </div>
                         </div>
@@ -145,29 +149,25 @@
                                 <input type="number" name="harga" class="form-control" id="colFormLabel" value="<?=$row['harga'];?>" >
                             </div>
                         </div>
+
                         <?php
                             }
                         ?>
-                        <p align="right">
-                        <input class="btn btn-primary" type="submit" value="SAVE" name="save"/>
-                        <a class="btn btn-danger" href="produk.php?status=0">Cancel</a>
-                        </p>
-                        </form> 
 
-                               
+                        <p align="right">
+                            <input class="btn btn-primary" type="submit" value="SAVE" name="save"/>
+                            <a class="btn btn-danger" href="produk.php?status=0">Cancel</a>
+                        </p>
+                        </form>    
                     </div>
-               
-                    <!-- End Content -->
-              
-      </div>
+                </div>
+            </div>
+        </div>
+        </div>
     </div>
-          
-    </div> <!-- /.row -->
-        </div> <!-- /.container -->
-            </div> <!-- /#services -->
 
 <?php
     include 'include/footer.php';
 ?>
+
 </body>
-</html>
--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <?php
     include 'admin/core/init.php';
     include 'include/header.php';
-  
     include 'admin/api/config/database.php';
 
-	session_start();
+	// Session
+    session_start();
+    if(!isset($_SESSION['login_mitra'])) {
+      header('location:index.php');
+    }else {
+      $login_mitra = $_SESSION['login_mitra'];
+    }
+
+    //Connect To Database
     $database = new Database();
     $db = $database->getConnection();
 ?>
+
+<!-- Content -->
 <body>
-    
-<div id="home">
+    <div id="home">
         <div class="site-header">
             <div class="top-header">
                 <div class="container">
@@ -18,11 +26,11 @@
                         <div class="col-md-6 col-sm-6">
                              <div class="left-header lead">
                                <img src="images/logoo2.png" width="280px" height="50px"><a href="index.html"></a>                            
-                            </div> <!-- /.left-header -->
-                        </div> <!-- /.col-md-6 -->
-                    </div> <!-- /.row -->
-                </div> <!-- /.container -->
-            </div> <!-- /.top-header -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="main-header">
                 <div class="container">
                     <div class="row">
@@ -34,9 +42,9 @@
                                     <li><a href="#portfolio">How To Use</a></li>
                                     <li><a href="#contact">Login</a></li>
                                 </ul>
-                            </div> <!-- /.menu -->
-                        </div> <!-- /.col-md-8 -->
-                    </div> <!-- /.row -->
+                            </div>
+                        </div>
+                    </div>
                     <div class="responsive-menu text-right visible-xs visible-sm">
                         <a href="#" class="toggle-menu fa fa-bars"></a>
                         <div class="menu">
@@ -46,14 +54,13 @@
                                     <li><a href="#portfolio">How To Use</a></li>
                                 <li><a href="#contact">Login</a></li>
                             </ul>
-                        </div> <!-- /.menu -->
-                    </div> <!-- /.responsive-menu -->
-                </div> <!-- /.container -->
-            </div> <!-- /.header -->
-        </div> <!-- /.site-header -->
-    </div> <!-- /#home -->
-
-   <div class="flexslider">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flexslider">
         <ul class="slides">
             <li>
                 <img src="images/banner07.jpg" alt="">
@@ -82,12 +89,13 @@
         </ul>
     </div>
 
+    <!-- About Us -->
     <div id="services" class="section-cotent">
         <div class="container">
             <div class="title-section text-center">
                 <h2>ABOUT US</h2>
                 <span></span>
-            </div> <!-- /.title-section -->
+            </div>
 			<p align ="justify">EZprint adalah sebuah aplikasi mobile yang mampu menyediakan jasa print online secara mudah, cepat dan efisien.  </p>
             <p align ="justify"> Printing adalah kegiatan yang sering dilakukan banyak orang, tanpa terkecuali mahasiswa. Tumpukan tugas-tugas kuliah memang kebanyakan berupa makalah atau essay sehingga dalam pengumpulannya harus di-print terlebih dahulu. Namun, apa jadinya jika tugas makalah selesai Jam 12 malam dan besuk pagi harus dikumpulkan sedangkan jasa print belum ada yang buka? 
             Inilah permasalahan yang sering dialami banyak anak kostan yang tidak punya sebuah printer. Berdasarkan survey kami didapatkan beberapa data berikut : </p>
@@ -96,17 +104,17 @@
             <p>•	Selain itu, bagi mahasiswa waktu adalah uang sehingga menunggu ngeprint baginya adalah kegiatan yang membosankan.  </p>
             <p>•	Apalagi ketepatan waktu adalah yang dipentingkan karena menyangkut masalah deadline yang bila terlambat akan dikenakan pengurangan nilai hingga diskualifikasi. </p>
             <p>•	Mahasiswa seringkali tidak memilki banyak waktu keluar kos dan tidak memiliki printer </p>
-            </div> <!-- /.row -->
-        </div> <!-- /.container -->
-    </div> <!-- /#services -->
+            </div>
+        </div>
+    </div>
 
+    <!-- How To Use -->
     <div id="portfolio" class="section-content">
         <div class="container">
             <div class="title-section text-center">
                 <h2>HOW TO USE</h2>
                 <span></span>
-            </div> <!-- /.title-section -->
-			
+            </div>
             <div class="row">
                 <div class="col-md-2 col-sm-6">
                     <div class="portfolio-thumb">
@@ -116,9 +124,9 @@
                                 <h4><a data-rel="lightbox" href="images/portfolio/item3.jpg"> </a></h4>
                                 <span>Sign Up</span>
                             </div>
-                        </div> <!-- /.overlay -->
-                    </div> <!-- /.portfolio-thumb -->
-                </div> <!-- /.col-md-3 -->
+                        </div>
+                    </div>
+                </div>
 				 <div class="col-md-2 col-sm-6">
                     <div class="portfolio-thumb">
                         <img src="images/6.jpg" alt="Portfolio Item 1">
@@ -127,9 +135,9 @@
                                 <h4><a data-rel="lightbox" href="images/6.jpg"> </a></h4>
                                 <span>Sign In</span>
                             </div>
-                        </div> <!-- /.overlay -->
-                    </div> <!-- /.portfolio-thumb -->
-                </div> <!-- /.col-md-3 -->
+                        </div>
+                    </div>
+                </div>
                 <div class="col-md-2 col-sm-6">
                     <div class="portfolio-thumb">
                         <img src="images/5.jpg" alt="Portfolio Item 1">
@@ -138,20 +146,20 @@
                                 <h4><a data-rel="lightbox" href="images/5.jpg"> </a></h4>
                                 <span>Pilihan Sesuai Lokasi</span>
                             </div>
-                        </div> <!-- /.overlay -->
-                    </div> <!-- /.portfolio-thumb -->
-                </div> <!-- /.col-md-3 -->
+                        </div>
+                    </div>
+                </div>
                 <div class="col-md-2 col-sm-6">
                     <div class="portfolio-thumb">
                         <img src="images/4.jpg" alt="Portfolio Item 2">
                         <div class="overlay">
                             <div class="inner">
-                                <h4><a data-rel="lightbox" href="images/4.jpg"> </a></h4>
+                                <h4><a data-rel="lightbox" href="images/4.jpg"></a></h4>
                                 <span>Pilihan Sesuai Kategori</span>
                             </div>
-                        </div> <!-- /.overlay -->
-                    </div> <!-- /.portfolio-thumb -->
-                </div> <!-- /.col-md-3 -->
+                        </div>
+                    </div>
+                </div>
                 <div class="col-md-2 col-sm-6">
                     <div class="portfolio-thumb">
                         <img src="images/8.jpg" alt="Portfolio Item 3">
@@ -160,30 +168,31 @@
                                 <h4><a data-rel="lightbox" href="images/8.jpg"> </a></h4>
                                 <span>Menu Drawer</span>
                             </div>
-                        </div> <!-- /.overlay -->
-                    </div> <!-- /.portfolio-thumb -->
-                </div> <!-- /.col-md-3 -->
+                        </div>
+                    </div>
+                </div>
                 <div class="col-md-2 col-sm-6">
                     <div class="portfolio-thumb">
                         <img src="images/9.jpg" alt="Portfolio Item 4">
                         <div class="overlay">
                             <div class="inner">
-                                <h4><a data-rel="lightbox" href="images/9.jpg"> </a></h4>
+                                <h4><a data-rel="lightbox" href="images/9.jpg"></a></h4>
                                 <span>Halaman Profile</span>
                             </div>
-                        </div> <!-- /.overlay -->
-                    </div> <!-- /.portfolio-thumb -->
-                </div> <!-- /.col-md-3 -->
-            </div> <!-- /.row -->
-        </div> <!-- /.container -->
-    </div> <!-- /#portfolio -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <!-- LOGIN -->
     <div id="contact" class="section-cotent">
         <div class="container">
             <div class="title-section text-center">
                 <h2>Login Mitra</h2>
                 <span></span>
-            </div> <!-- /.title-section -->
+            </div>
             <div class="row-center">
 				<div class="col-md-4"></div>
                 <div class="col-md-4 col-sm-8">
@@ -200,22 +209,20 @@
                         <input class="mainBtn" type="submit" name="login" value="Log in" >
                         </form>
                     </div>
-					<p class="full-row">
+					    <p class="full-row">
                             <label for="name-id"> </label>
                         </p>
-						<p class="full-row">
+					    <p class="full-row">
                             <label for="name-id">Don't have an account ? <a href = "signup.php"> Sign up </a> </label>
                         </p>
-                </div> <!-- /.col-md-4 -->
-				<div class="col-md-4"></div>
-                </div> <!-- /.col-md-3 -->
-            </div> <!-- /.row -->
-        </div> <!-- /.container -->
-    </div> <!-- /#contact -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> 
 
 <?php
     include 'include/footer.php';
 ?>
 
 </body>
-</html>
